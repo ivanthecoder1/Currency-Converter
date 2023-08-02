e636ee03e2dfec00001c17ac789a5dc1d18458c6
Ivan Zhang <ivanzhang0111@gmail.com>
2023-08-02 14:25:29 -0400

    set up node, install packages

---
+++ app.js
@@ -0,0 +1,10 @@
+// Utilize prompt sync for getting user input
+
+// Utilize NodeJS Currency Converter to convert a currency to another currency
+
+// Ask user what currency they are converting from
+// Ask user how much money they have
+// Ask user for target currency 
+const prompt = require('prompt-sync')();
+let initial_curr = prompt('What currency are you converting from?: ')
+
